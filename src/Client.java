b8f299ed95ef5927de04eb5858bbc8437439cd0e
lewys <lewysward99@gmail.com>
2019-01-31 21:48:09 +0000

    Added basic echo client/server

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client {
     public static void main(String[] args) {
+        try {
+            String host = args[0];
+            int port = Integer.parseInt(args[1]);
+
+            Socket socket = new Socket(host, port);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+            String s;
+            while ((s = stdIn.readLine()) != null) {
+                out.println(s);
+                System.out.println("Echo: " + in.readLine());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,44 @@
-public class Server {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    private static final int PORT = 30751;
+
+    public static void main(String[] args) {
+        try {
+            //Creates server socket, client socket, and output and input objects
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            Socket clientSocket;
+            PrintWriter out;
+            BufferedReader in;
+
+            //Loops and listens for incoming connections
+            while (true) {
+                //Accepts an incoming connection
+                clientSocket = serverSocket.accept();
+
+                //Sets the input and output streams to the connected client socket
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                //Reads input from the client and echos the data as a string
+                String s;
+                while ((s = in.readLine()) != null) {
+                    out.println(s);
+                    System.out.println("Received: " + s);
+                }
+
+                //closes all streams
+                out.close();
+                in.close();
+                clientSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
