feab7e2186fa96f01c39dbcb2d2a4dfa0a646044
locw <lewysward99@gmail.com>
2019-02-02 15:04:50 +0000

    Updated echo client/server to use UDP instead of TCP

--- src/Client.java
+++ src/Client.java
@@ -15,19 +15,28 @@ public class Client {
 
     public static void main(String[] args) {
         try {
+            //Initialises input stream for user input
             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+            //Creates UDP socket for client-server communication
             DatagramSocket clientSocket = new DatagramSocket();
+
+            //Gets localhost as ip address
             InetAddress ip = InetAddress.getLocalHost();
 
+            //Arrays for sending and receiving data
             byte[] sendData = new byte[DATA_SIZE];
             byte[] receiveData = new byte[DATA_SIZE];
 
+
             String message;
             while ((message = stdIn.readLine()) != null) {
+                //Reads in message from user and sends it to server
                 sendData = message.getBytes();
                 DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ip, PORT);
                 clientSocket.send(sendPacket);
 
+                //Receives modified data from server and displays it
                 DatagramPacket receivedPacket = new DatagramPacket(receiveData, receiveData.length);
                 clientSocket.receive(receivedPacket);
                 String serverMessage = new String(receivedPacket.getData());
